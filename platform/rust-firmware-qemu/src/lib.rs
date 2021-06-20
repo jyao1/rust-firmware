d3e1056d3e3791d427a1fc33fc76d7be092d8fb2
Xiaoyu Lu <xiaoyux.lu@intel.com>
2021-06-20 12:44:39 +0800

    add rust-fsp and platform/rust-firmware-qemu
    Signed-off-by: Xiaoyu Lu <xiaoyux.lu@intel.com>

---
+++ platform/rust-firmware-qemu/src/lib.rs
@@ -0,0 +1,5 @@
+// Copyright (c) 2021 Intel Corporation
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+
+pub mod fsp_data;
---
+++ rust-fsp/src/lib.rs
@@ -0,0 +1,17 @@
+// Copyright (c) 2020 Intel Corporation
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+
+#![no_std]
+
+pub mod fsp_upd_header;
+pub mod fsp_t_upd;
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn dump_fsp() {
+
+    }
+}
