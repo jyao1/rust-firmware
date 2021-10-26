f07179511118298167f0888074b835320c69077a
Xiaoyu Lu <xiaoyux.lu@intel.com>
2021-10-26 16:48:15 +0800

    rust-fsp-wrapper: move fsp calls from rust-ipl.
    TBD: Need refactor.
    
    Signed-off-by: Xiaoyu Lu <xiaoyux.lu@intel.com>

---
+++ rust-fsp-wrapper/src/asm/mod.rs
@@ -0,0 +1,47 @@
+// Copyright (c) 2021 Intel Corporation
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+use x86::dtables;
+
+global_asm!(include_str!("thunk64to32.s"));
+global_asm!(include_str!("read_write_gdtr.s"));
+global_asm!(include_str!("read_write_idtr.s"));
+
+extern "win64" {
+    fn AsmExecute32BitCode(
+        function_entry: usize,
+        param1: usize,
+        param2: usize,
+        gdtr: usize,
+    ) -> usize;
+    fn lidt_call(idtr: usize);
+    fn sgdt_call(gdtr: usize);
+}
+
+pub fn execute_32bit_code(entry_point: usize, param1: usize, param2: usize) -> usize {
+    log::trace!(
+        "execute 32bit code - entry: 0x{:x}, param1: 0x{:x}, param2: 0x{:x}\n",
+        entry_point,
+        param1,
+        param2
+    );
+    unsafe {
+        // Let FSP to setup the IDT
+        let idtr_null = x86::dtables::DescriptorTablePointer {
+            base: core::ptr::null::<usize>(),
+            limit: 0xffff,
+        };
+        lidt_call(&idtr_null as *const dtables::DescriptorTablePointer<usize> as usize);
+
+        // Use current GDT
+        let gdtr = dtables::DescriptorTablePointer::<usize>::default();
+        sgdt_call(&gdtr as *const dtables::DescriptorTablePointer<usize> as usize);
+
+        AsmExecute32BitCode(
+            entry_point,
+            param1,
+            param2,
+            &gdtr as *const dtables::DescriptorTablePointer<usize> as usize,
+        )
+    }
+}
---
+++ rust-fsp-wrapper/src/memslice.rs
@@ -0,0 +1,78 @@
+// Copyright (c) 2021 Intel Corporation
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+
+// TBD: Duplicated file. need be removed.
+
+use rust_firmware_layout::build_time::*;
+use rust_firmware_layout::runtime::*;
+use rust_firmware_layout::fsp_build_time::*;
+
+#[allow(dead_code)]
+pub enum SliceType {
+    FirmwareIplSlice,
+    FirmwarePayloadSlice,
+    FirmwareFspTSlice,
+    FirmwareFspMSlice,
+    FirmwareFspSSlice,
+    RuntimePayloadSlice,
+    RuntimePayloadHobSlice,
+    RuntimeStackSlice,
+    RuntimeHeapSlice,
+}
+
+pub fn get_mem_slice<'a>(t: SliceType) -> &'a [u8] {
+    unsafe {
+        match t {
+            SliceType::FirmwareIplSlice => core::slice::from_raw_parts(
+                LOADED_IPL_BASE as *const u8,
+                FIRMWARE_IPL_SIZE as usize,
+            ),
+            SliceType::FirmwarePayloadSlice => core::slice::from_raw_parts(
+                LOADED_PAYLOAD_BASE as *const u8,
+                FIRMWARE_PAYLOAD_SIZE as usize,
+            ),
+            SliceType::FirmwareFspTSlice => core::slice::from_raw_parts(
+                LOADED_FSP_T_BASE as *const u8,
+                FIRMWARE_FSP_T_SIZE as usize,
+            ),
+            SliceType::FirmwareFspMSlice => core::slice::from_raw_parts(
+                LOADED_FSP_M_BASE as *const u8,
+                FIRMWARE_FSP_M_SIZE as usize,
+            ),
+            SliceType::FirmwareFspSSlice => core::slice::from_raw_parts(
+                LOADED_FSP_S_BASE as *const u8,
+                FIRMWARE_FSP_S_SIZE as usize,
+            ),
+            _ => {
+                panic!("not support")
+            }
+        }
+    }
+}
+
+pub fn get_dynamic_mem_slice_mut<'a>(t: SliceType, base_address: usize) -> &'a mut [u8] {
+    unsafe {
+        match t {
+            SliceType::RuntimePayloadHobSlice => core::slice::from_raw_parts_mut(
+                base_address as *const u8 as *mut u8,
+                RUNTIME_HOB_SIZE as usize,
+            ),
+            SliceType::RuntimePayloadSlice => core::slice::from_raw_parts_mut(
+                base_address as *const u8 as *mut u8,
+                RUNTIME_PAYLOAD_SIZE as usize,
+            ),
+            SliceType::RuntimeStackSlice => core::slice::from_raw_parts_mut(
+                base_address as *const u8 as *mut u8,
+                RUNTIME_STACK_SIZE as usize,
+            ),
+            SliceType::RuntimeHeapSlice => core::slice::from_raw_parts_mut(
+                base_address as *const u8 as *mut u8,
+                RUNTIME_HEAP_SIZE as usize,
+            ),
+            _ => {
+                panic!("not support")
+            }
+        }
+    }
+}
